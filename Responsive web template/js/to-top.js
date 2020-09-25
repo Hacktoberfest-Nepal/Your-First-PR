89d2c8b726c7436659c402423ede1ab9a5eb7e54
gigabite-pro <rajmachawal19@gmail.com>
2020-09-25 11:09:54 +0530

    removed all files

--- Responsive web template/js/to-top.js
+++
@@ -1,9 +0,0 @@
-const toTop = document.querySelector(".to-top");
-
-window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
-    toTop.classList.add("active");
-  } else {
-    toTop.classList.remove("active");
-  }
-})
